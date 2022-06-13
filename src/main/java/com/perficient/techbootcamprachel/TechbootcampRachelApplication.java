279ff577648cec4b6a9646246ab27c89ddd548dc
rekennelly <rekennelly@gmail.com>
2022-06-13 16:58:38 -0400

    updates & import into STS

---
+++ src/main/java/com/perficient/techbootcamprachel/TechbootcampRachelApplication.java
@@ -0,0 +1,14 @@
+package com.perficient.techbootcamprachel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampRachelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampRachelApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/perficient/techbootcamprachel/TechbootcampRachelApplicationTests.java
@@ -0,0 +1,18 @@
+package com.perficient.techbootcamprachel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbootcampRachelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
