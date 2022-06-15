a03ada4a20c3d0b9069d3f7bd9dcbcee487b6631
rekennelly <rekennelly@gmail.com>
2022-06-15 15:17:25 -0400

    SQL issues fixed

--- src/test/java/com/perficient/techbootcamprachel/TechbootcampRachelApplicationTests.java
+++ src/test/java/com/perficient/techbootcamprachel/TechbootcampRachelApplicationTests.java
@@ -6,13 +6,25 @@ import java.io.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TechbootcampRachelApplicationTests {
+	private static final Logger logger = LoggerFactory.getLogger(TechbootcampRachelApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.debug("DEBUG");
+		logger.trace("TRACE");
+		logger.info("INFO");
+		logger.warn("WARN");
+		logger.error("ERROR");
+		
+		String foo = "foo";
+		logger.debug("foo is " + foo); // do not do string concat (expensive operation)
+		logger.debug("foo is {}", foo);
 	}
 
 }
